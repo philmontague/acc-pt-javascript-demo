18d663fee3a5b4d6da002739d6739398f3787db0
dsl2022 <2015rpro@gmail.com>
2023-08-31 19:30:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttonBc =document.querySelector(".btn-bc");
+const buttonTitle =document.querySelector("#btn-title");
+const buttonCir =document.querySelector("#btn-circles");
+const body = document.querySelector("body");
+const mainTitle = document.querySelector("#main-title");
+
+
+console.log({buttonBc,buttonCir,buttonTitle})
+
+buttonBc.addEventListener('click',()=>{
+    body.style.backgroundColor = "yellow";
+})
+
+buttonTitle.addEventListener("click",()=>{
+    mainTitle.textContent= "I am hungry! 🦞"
+})
+
+buttonCir.addEventListener("click",()=>{
+    const circle = document.createElement('div');
+    circle.style.width = "200px";
+    circle.style.height = "200px";
+    circle.style.borderRadius = "100%";
+    circle.style.border = "1px solid black";
+    body.append(circle);
+})
